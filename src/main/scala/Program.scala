4d16781cdec6d97c4d5c13a86fe3820a4ee64237
Santiago Suarez <suarezsantiagoperez@gmail.com>
2025-08-21 17:13:36 -0500

    Removed commented code and updated sample formula

--- src/main/scala/Program.scala
+++ src/main/scala/Program.scala
@@ -3,22 +3,7 @@ import Literal._
 object Program {
 
   def main(args: Array[String]): Unit = {
-    /*
-    FormulaParser("- a & b") match {
-      case Right(f) => println(s"Parsed AST: $f")
-      case Left(er) => println(s"Did not pass: $er")
-    }
-    FormulaParser("-(p || q) <-> r") match {
-      case Right(f) => println(s"Parsed AST: $f")
-      case Left(er) => println(s"Did not pass: $er")
-    }
-    FormulaParser("a & b || c -> d <-> -e") match {
-      case Right(f) => println(s"Parsed AST: $f")
-      case Left(er) => println(s"Did not pass: $er")
-    }
-     */
-
-    FormulaParser("a -> b") match {
+    FormulaParser("(a & b) || -c") match {
       case Right(f) =>
         val t                            = new TseitinTransformer()
         val clauses: List[List[Literal]] = t.encode(f)
